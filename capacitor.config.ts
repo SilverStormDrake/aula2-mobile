0746c8fdb16ff779f583127857b7be1bb0c7ded3
André Luiz <andrelu0022@gmail.com>
2023-03-02 09:18:18 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'aula2-mod',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
